df17f145e2518cb6fe4849a379f529e56cd96f48
KomugiKuru <komugi445@gmail.com>
2025-11-29 11:25:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const imge = document.getElementById('ima');
 
 btn.addEventListener('click',function(){
     if (imge.style.display === 'none') {
+        content.style.backgroundColor = "rgba(92, 55, 128, 1)"
         imge.style.display = 'block';  // 表示する 表示するときはブロック（？）とかうってね！？今非公開だし！！！
     } else {
+        content.style.backgroundColor = "rgb(172, 101, 238)"
         imge.style.display = 'none';   // 隠す
     }
 
